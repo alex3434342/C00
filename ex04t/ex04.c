a590e3e1437e34f8d4ed32ae84ecc7ec19c14eb3
alex3434342 <arsn00778@yandex.ru>
2021-09-10 16:52:21 +0300

    ex04

---
+++ ex04t/ex04.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	float result;
+
+	result=(89 + 872)/(34*34*12.0)-(0.87 - (-0.4002 * 12/7.6)*(-0.4002 * 12/7.6)*(-0.4002 * 12/7.6))/(3.0);
+	printf("%f" , result);
+	return (0);
+}
