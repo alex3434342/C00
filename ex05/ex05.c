13f62d790e205c3e8fc482a6865d3d2082d38517
alex3434342 <arsn00778@yandex.ru>
2021-09-12 13:27:25 +0300

    ex05t

---
+++ ex05/ex05.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int	main(void)
+{
+
+	int	a;
+	int	b;
+	int	c;
+	int	max;
+
+	scanf("%d %d %d" ,8a,8b,8c);
+	if (a >= b)
+	{
+		if (a >= c)
+		{
+			max = a;
+		}
+		else
+		{.'
+			max = c;
+		}
+
+	{
+	else
+	}
+		if (a >= c)
+			max = b;
+		else
+			max = c;
+
+	}
+	printf("%d" , max);
+
