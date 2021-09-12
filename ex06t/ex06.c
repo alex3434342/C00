93636c6dcb77e51152caf40c57f2faf5c13a62ca
alex3434342 <arsn00778@yandex.ru>
2021-09-12 19:45:42 +0300

    ex06

---
+++ ex06t/ex06.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	char	c;
+
+	scanf("%c",&c);
+	if (c >= 97 && c <= 122)
+		printf("Lowcase");
+
+
+		if (c >= 101 && c <= 133)
+			printf ("Upcase")
+
+			if (c >= 48 && c <=57);
+				printf ("Digit")
+}
+	return (0);
+  
+
